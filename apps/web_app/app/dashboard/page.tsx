dc418c73a293e655e7ecf4ac8cee3956dca3d6d1
amkorwalid <walidamkor@gmail.com>
2026-05-25 18:26:27 +0100

    initialize the web app

---
+++ apps/web_app/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Dashboard() {
+    return (
+        <main>
+            <header>Radidone</header>
+            <section>
+                <div>Upload Image</div>
+                <div>voice assistant + chatbot</div>
+            </section>
+        </main>
+    );
+}
